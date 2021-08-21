5db5032ad0eb6d370116ed21b37a26dd244e85fe
Sam Gammon <samuel.gammon@gmail.com>
2021-08-21 12:59:52 -0700

    Add basic aliases

---
+++ zsh/configs/aliases.zsh
@@ -0,0 +1,9 @@
+
+alias c=clear
+alias gs="clear ; git status -sb"
+alias ga="clear ; git add -p"
+alias gca="git commit --amend"
+alias m=make
+alias k=kubectl
+alias gdc="git diff --cached"
+alias g=git
