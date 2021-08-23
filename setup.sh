e775edba5265710f72a0c447d7052a9854a082f9
Sam Gammon <samuel.gammon@gmail.com>
2021-08-22 22:40:38 -0700

    Improvements to setup script

--- setup.sh
+++ setup.sh
@@ -13,14 +13,33 @@ rcup;
 echo ""; echo "--- Setting up credentials...";
 
 if [[ -z "${GOOGLE_CREDENTIALS}" ]]; then
+  echo "No Google credentials detected.";
+else
+  echo "- Installing Google credentials...";
   mkdir -p "$HOME/.config/gcloud";
   echo "${GOOGLE_CREDENTIALS}" > "$HOME/.config/gcloud/application_default_credentials.json";
 fi
 
 if [[ -z "${BUILDBUDDY_CERT}" ]]; then
+  echo "No Buildbuddy certificate detected.";
+else
+  echo "- Installing Buildbuddy certificate...";
   echo "${BUILDBUDDY_CERT}" > "$HOME/buildbuddy-cert.pem";
 fi
 
 if [[ -z "${BUILDBUDDY_KEY}" ]]; then
+  echo "No Buildbuddy key detected.";
+else
+  echo "- Installing Buildbuddy key...";
   echo "${BUILDBUDDY_KEY}" > "$HOME/buildbuddy-key.pem";
+fi
+
+if [[ -z "${SSHKEY}" ]]; then
+  echo "No SSH key detected.";
+else
+  echo "- Installing SSH key...";
+  mkdir -p $HOME/.ssh/id_rsa;
+  echo "${SSHKEY}" > "$HOME/.ssh/id_rsa";
+  chmod 600 $HOME/.ssh/id_rsa;
+  chown dev:engineering $HOME/.ssh/id_rsa;
 fi
